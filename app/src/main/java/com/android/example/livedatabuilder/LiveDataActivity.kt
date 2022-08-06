cadcd70d7af0277f82a3113306fb4c569867e5dd
casanovaP <85435049+casanovaP@users.noreply.github.com>
2022-08-06 16:24:40 +0200

    live data tests app understanding live data, ViewModel, observer ..etc

---
+++ app/src/main/java/com/android/example/livedatabuilder/LiveDataActivity.kt
@@ -0,0 +1,31 @@
+package com.android.example.livedatabuilder
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.android.example.livedata.R
+import com.android.example.livedata.databinding.ActivityLivedataBinding
+
+class LiveDataActivity : AppCompatActivity() {
+
+    // Obtain ViewModel
+    private val viewmodel: LiveDataViewModel by viewModels { LiveDataVMFactory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Obtain binding object using the Data Binding library
+        val binding = DataBindingUtil.setContentView<ActivityLivedataBinding>(
+            this, R.layout.activity_livedata
+        )
+
+        // Set the LifecycleOwner to be able to observe LiveData objects
+        binding.lifecycleOwner = this
+
+        // Bind ViewModel
+        binding.viewmodel = viewmodel
+    }
+}
+
+
